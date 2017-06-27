8ca0a63626255d3f52a11798fccb7eebbea4068a
rule1001 <hello@rule1001.com>
2017-06-27 19:40:30 +0100

    emoji search implemented and tested. removed destructive element and improved match filter function

--- src/search.js
+++ src/search.js
@@ -1,28 +1,18 @@
 import React from 'react';
 
 class Search extends React.Component {
-    constructor (props) {
-        super(props);
-    this.changeHandler = this.changeHandler.bind(this)
-    }
-    render(){
+    render() {
         return (
             <div>
                 <form >
-                    <input 
-                    type="text" 
-                    onChange={this.changeHandler}
-                   />
+                    <input
+                        type="text"
+                        onChange={this.props.onChange}
+                    />
                 </form>
             </div>
-        )
+        );
     }
-  changeHandler (event) {
-    // console.log(event.target.value)
-    event.preventDefault();
-    const stringMatch = event.target.value
-    this.props.emojiFilter(this.props.emoji, stringMatch)
-  }
 }
 
 export default Search;
