330dd9c47b56d508c93433301dad54ae1c9ab4d0
BIWhitfield <beniwhitfield@gmail.com>
2017-06-27 15:15:55 +0100

    [emojiList] add hard coded emoji array to test, add map to display emojis on screen

--- src/emojiList.js
+++ src/emojiList.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import Emoji from './emojis';
+import React from "react";
+import Emoji from "./emojis";
+import App from "./app";
 
+const emojis = [
+  {
+    title: "100",
+    symbol: "💯",
+    keywords: "hundred points symbol symbol wow wow win win perfect perfect parties parties"
+  },
+  {
+    title: "1234",
+    symbol: "🔢",
+    keywords: "input symbol for numbers symbol"
+  },
+  {
+    title: "Grinning",
+    symbol: "😀",
+    keywords: "grinning face happy smiley emotion emotion"
+  },
+  {
+    title: "Grimacing",
+    symbol: "😬",
+    keywords: "grimacing face silly smiley emotion emotion selfie selfie"
+  }
+];
 
 class EmojiList extends React.Component {
-    render() {
-        return(
-            <div>
-                <h3>emoji list</h3>
-                <Emoji/>
-                <Emoji/>
-                <Emoji/>
-                <Emoji/>
-                <Emoji/>
-                <Emoji/>
-            </div>
-        )
+  render() {
+    return (
+      <div>
+        <h2>list</h2>
+        {emojis.map(function(emoji) {
+          return <Emoji 
+          symbol={emoji.symbol} 
+          title={emoji.title} />;
+        })}
 
-
-    }
-
-
+      </div>
+    );
+  }
 }
 
 export default EmojiList;
