acbadd7cb0e605da9918d7918414e1679bdf1b48
BIWhitfield <beniwhitfield@gmail.com>
2017-06-27 15:15:14 +0100

    [app] update app title

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import EmojiList from './emojiList';
-import Search from './search';
+import React from "react";
+import EmojiList from "./emojiList";
+import Search from "./search";
 
 class App extends React.Component {
-    render () {
-        return (
-            <div>
-                <h3>welcome to my emoji app</h3>
-                <Search />
-                <EmojiList />
-            </div>
-        );
-
-    }
+  // constructor (props) {
+  //     super(props);
+  //     this.state = {
+  //         emoji: this.props.emoji
+  //     }
+  // }
+  render() {
+    return (
+      <div>
+        <h3>Search Emoji's</h3>
+        <Search />
+        <EmojiList />
+      </div>
+    );
+  }
 }
 
 export default App;
-
